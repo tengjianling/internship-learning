7fa0b2658b36c9c022db10382daf24db86dea8cb
Teng Jian Ling <tengjianling98@gmail.com>
2021-12-06 00:26:01 +0800

    add ruby tutorial

---
+++ ruby-tutorial/app/controllers/travel_controller.rb
@@ -0,0 +1,48 @@
+class TravelController < ApplicationController
+    def index
+    end
+
+    def request_api(url)
+        response = Excon.get(
+            url,
+            headers: {
+                'X-RapidAPI-Host' => URI.parse(url).host,
+                'X-RapidAPI-Key' => ENV.fetch('RAPID_API_KEY')
+            }
+        )
+
+        return nil if response.status != 200
+
+        JSON.parse(response.body)
+    end
+
+    def find_country(name)
+        request_api(
+            "https://restcountries-v1.p.rapidapi.com/name/#{URI.encode(name)}"
+        )
+    end
+
+    def find_weather(city, country_code)
+        query = URI.encode("#{city}", "#{country_code}")
+
+        request_api(
+            "https://community-open-weather-map.p.rapidapi.com/forecast?q=#{query}"
+        )
+    end
+
+    def search
+        countries = find_country(params[:country])
+
+        unless countries
+            flash[:alert] = 'Country not found'
+            return render action: :index
+        end
+
+        @country = countries.first
+        @weather = find_weather(@country['capital']), @country['alpha2code']
+
+    end 
+
+    
+
+end
